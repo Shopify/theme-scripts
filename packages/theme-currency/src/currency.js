735435884be819b932fdc8bf08fe3294dd0a954c
Chris Berthe <chris.berthe@shopify.com>
2018-05-28 16:01:09 -0400

    Fix broken Jest tests

--- packages/theme-currency/src/currency.js
+++ packages/theme-currency/src/currency.js
@@ -54,14 +54,11 @@ export function formatMoney(cents, format) {
     case 'amount_no_decimals':
       value = formatWithDelimiters(cents, 0);
       break;
-    case 'amount_with_space_separator':
-      value = formatWithDelimiters(cents, 2, ' ', '.');
+    case 'amount_with_comma_separator':
+      value = formatWithDelimiters(cents, 2, '.', ',');
       break;
     case 'amount_no_decimals_with_comma_separator':
-      value = formatWithDelimiters(cents, 0, ',', '.');
-      break;
-    case 'amount_no_decimals_with_space_separator':
-      value = formatWithDelimiters(cents, 0, ' ');
+      value = formatWithDelimiters(cents, 0, '.', ',');
       break;
   }
 
