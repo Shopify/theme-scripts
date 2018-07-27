327cee9a6f03dc57fc79a84809399059acea12d9
Thomas Tau <tau.thomas01@gmail.com>
2018-07-27 13:43:12 -0400

    Fixed test cases name and prettier quotes config

--- packages/theme-a11y/theme-a11y.test.js
+++ packages/theme-a11y/theme-a11y.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import path from "path";
+import path from 'path';
 import {
   pageLinkFocus,
   focusHash,
@@ -10,22 +10,22 @@ import {
   focusable,
   trapFocus,
   removeTrapFocus
-} from "./theme-a11y";
+} from './theme-a11y';
 
-describe("pageLinkFocus()", () => {
+describe('pageLinkFocus()', () => {
   beforeEach(() => {
     document.body.innerHTML =
       '<div id="nonFocusableElement"></div>' +
       '<button id="focusableElement" tabIndex=3 class="myClass">';
   });
 
-  test("is a function exported by theme-a11y.js", () => {
-    expect(typeof pageLinkFocus).toBe("function");
+  test('is a function exported by theme-a11y.js', () => {
+    expect(typeof pageLinkFocus).toBe('function');
   });
 
   test("adds a 'tabindex=-1' attribute", () => {
-    const nonFocusableElement = document.getElementById("nonFocusableElement");
-    const focusableElement = document.getElementById("focusableElement");
+    const nonFocusableElement = document.getElementById('nonFocusableElement');
+    const focusableElement = document.getElementById('focusableElement');
 
     pageLinkFocus(nonFocusableElement);
     expect(nonFocusableElement.tabIndex).toBe(-1);
@@ -34,8 +34,8 @@ describe("pageLinkFocus()", () => {
     expect(focusableElement.tabIndex).toBe(-1);
   });
 
-  test("reserves the original tabindex value in a data-tabIndex attribute", () => {
-    const focusableElement = document.getElementById("focusableElement");
+  test('reserves the original tabindex value in a data-tabIndex attribute', () => {
+    const focusableElement = document.getElementById('focusableElement');
 
     focusableElement.tabIndex = 3;
     pageLinkFocus(focusableElement);
@@ -43,8 +43,8 @@ describe("pageLinkFocus()", () => {
     expect(parseInt(focusableElement.dataset.tabIndex)).toBe(3);
   });
 
-  test("adds a focus state", () => {
-    const focusableElement = document.getElementById("focusableElement");
+  test('adds a focus state', () => {
+    const focusableElement = document.getElementById('focusableElement');
 
     pageLinkFocus(focusableElement);
 
@@ -52,24 +52,24 @@ describe("pageLinkFocus()", () => {
   });
 
   test("adds the default class name 'js-focus-hidden'", () => {
-    const focusableElement = document.getElementById("focusableElement");
-    const defaultClass = "js-focus-hidden";
+    const focusableElement = document.getElementById('focusableElement');
+    const defaultClass = 'js-focus-hidden';
     pageLinkFocus(focusableElement);
 
     expect(focusableElement.classList.contains(defaultClass)).toBeTruthy();
   });
 
-  test("adds an overrided class name", () => {
-    const focusableElement = document.getElementById("focusableElement");
-    const customClass = "custom-class";
+  test('adds an overrided class name', () => {
+    const focusableElement = document.getElementById('focusableElement');
+    const customClass = 'custom-class';
     pageLinkFocus(focusableElement, { className: customClass });
 
     expect(focusableElement.classList.contains(customClass)).toBeTruthy();
   });
 
-  test("resets the element to its original state on blur", () => {
-    const focusableElement = document.getElementById("focusableElement");
-    const nonFocusableElement = document.getElementById("nonFocusableElement");
+  test('resets the element to its original state on blur', () => {
+    const focusableElement = document.getElementById('focusableElement');
+    const nonFocusableElement = document.getElementById('nonFocusableElement');
     const clone = focusableElement.cloneNode(true);
 
     pageLinkFocus(focusableElement);
@@ -79,30 +79,30 @@ describe("pageLinkFocus()", () => {
   });
 });
 
-describe("focusHash()", () => {
+describe('focusHash()', () => {
   beforeEach(() => {
     document.body.innerHTML =
       '<div id="nonFocusableElement"></div>' +
       '<button id="focusableElement" tabIndex=3 class="myClass">';
   });
 
-  test("is a function exported by theme-a11y.js", () => {
-    expect(typeof focusHash).toBe("function");
+  test('is a function exported by theme-a11y.js', () => {
+    expect(typeof focusHash).toBe('function');
   });
 
-  test("focuses an element whose ID is specified in the URL hash", () => {
-    const focusableElement = document.getElementById("focusableElement");
+  test('focuses an element whose ID is specified in the URL hash', () => {
+    const focusableElement = document.getElementById('focusableElement');
 
-    window.location.hash = "focusableElement";
+    window.location.hash = 'focusableElement';
     focusHash();
 
     expect(document.activeElement).toBe(focusableElement);
   });
 
-  test("does not change the focus if element specified does not exist", () => {
-    const focusableElement = document.getElementById("focusableElement");
+  test('does not change the focus if element specified does not exist', () => {
+    const focusableElement = document.getElementById('focusableElement');
 
-    window.location.hash = "otherElement";
+    window.location.hash = 'otherElement';
     pageLinkFocus(focusableElement);
     focusHash();
 
@@ -110,19 +110,19 @@ describe("focusHash()", () => {
   });
 });
 
-describe("bindInPageLinks()", () => {
+describe('bindInPageLinks()', () => {
   beforeEach(() => {
     document.body.innerHTML =
       '<a id="link" href="#title"></a>' + '<h1 id="title">Title</h1>';
   });
 
-  test("is a function exported by theme-a11y.js", () => {
-    expect(typeof bindInPageLinks).toBe("function");
+  test('is a function exported by theme-a11y.js', () => {
+    expect(typeof bindInPageLinks).toBe('function');
   });
 
   test("adds an event handler that focuses the element referred to in an <a> element w/ a href='#...' when it is clicked", () => {
-    const link = document.getElementById("link");
-    const title = document.getElementById("title");
+    const link = document.getElementById('link');
+    const title = document.getElementById('title');
 
     bindInPageLinks();
 
--- packages/theme-product/theme-product.test.js
+++ packages/theme-product/theme-product.test.js
@@ -4,23 +4,15 @@
 import {
   getVariant,
   createOptionArrayFromOptionCollection
-} from "./theme-product";
-import { getProductJsonMock } from "./__mocks__/product";
+} from './theme-product';
+import productJson from './__fixtures__/product.json';
 
-describe("getVariant()", () => {
-  let productJson;
-
-  beforeEach(() => {
-    let markup = getProductJsonMock();
-    document.body.innerHTML = `<script id="productJson" type="application/json">${markup}</script>`;
-    productJson = JSON.parse(document.getElementById("productJson").innerHTML);
+describe('getVariant()', () => {
+  test('is a function exported by theme-product.js', () => {
+    expect(typeof getVariant).toBe('function');
   });
 
-  test("is a function exported by theme-product.js", () => {
-    expect(typeof getVariant).toBe("function");
-  });
-
-  test("throws an error if parameters are missing", () => {
+  test('throws an error if parameters are missing', () => {
     expect(() => {
       getVariant();
     }).toThrow();
@@ -34,18 +26,18 @@ describe("getVariant()", () => {
     }).toThrow();
   });
 
-  test("throws an error if product json object is empty", () => {
+  test('throws an error if product json object is empty', () => {
     expect(() => {
       getVariant({}, 6908023078973);
     }).toThrow();
   });
 
-  test("returns a product variant object when called with valid arguments", () => {
+  test('returns a product variant object when a match is found', () => {
     const variant = getVariant(productJson, 6908023078973);
     expect(variant).toEqual(productJson.variants[0]);
   });
 
-  test("returns an empty object when called with arguments with no succesful matches", () => {
+  test('returns an empty object when called with arguments with no succesful matches', () => {
     const variant = getVariant(productJson, 6909083098073);
     expect(variant).toEqual({});
   });
@@ -58,12 +50,12 @@ describe("getVariant()", () => {
   test("returns a product variant object when called with collection's options with 'name' and 'value' keys", () => {
     const collections = [
       {
-        name: "Size",
-        value: "36"
+        name: 'Size',
+        value: '36'
       },
       {
-        name: "Color",
-        value: "Black"
+        name: 'Color',
+        value: 'Black'
       }
     ];
 
@@ -71,15 +63,15 @@ describe("getVariant()", () => {
     expect(variant).toEqual(productJson.variants[0]);
   });
 
-  test("returns an empty object when called with a collection of options with invalid values", () => {
+  test('returns an empty object when called with a collection of options with invalid values', () => {
     const collections = [
       {
-        name: "Size",
-        value: "10"
+        name: 'Size',
+        value: '10'
       },
       {
-        name: "Color",
-        value: "Purple"
+        name: 'Color',
+        value: 'Purple'
       }
     ];
 
@@ -87,34 +79,26 @@ describe("getVariant()", () => {
     expect(variant).toEqual({});
   });
 
-  test("returns a product variant object when called with an array of values", () => {
-    const arrayValues = ["38", "Black"];
+  test('returns a product variant object when called with an array of values', () => {
+    const arrayValues = ['38', 'Black'];
     const variant = getVariant(productJson, arrayValues);
     expect(variant).toEqual(productJson.variants[2]);
   });
 
-  test("returns an empty object when when called with an array of invalid values", () => {
-    const arrayValues = ["45", "Black"];
+  test('returns an empty object when when called with an array of invalid values', () => {
+    const arrayValues = ['45', 'Black'];
 
     const variant = getVariant(productJson, arrayValues);
     expect(variant).toEqual({});
   });
 });
 
-describe("createOptionArrayFromOptionCollection", () => {
-  let productJson;
-
-  beforeEach(() => {
-    let markup = getProductJsonMock();
-    document.body.innerHTML = `<script id="productJson" type="application/json">${markup}</script>`;
-    productJson = JSON.parse(document.getElementById("productJson").innerHTML);
+describe('createOptionArrayFromOptionCollection', () => {
+  test('is a function exported by theme-product.js', () => {
+    expect(typeof createOptionArrayFromOptionCollection).toBe('function');
   });
 
-  test("is a function exported by theme-product.js", () => {
-    expect(typeof createOptionArrayFromOptionCollection).toBe("function");
-  });
-
-  test("throws an error if parameters are missing", () => {
+  test('throws an error if parameters are missing', () => {
     expect(() => {
       createOptionArrayFromOptionCollection();
     }).toThrow();
@@ -124,7 +108,7 @@ describe("createOptionArrayFromOptionCollection", () => {
     }).toThrow();
   });
 
-  test("throws an error if the second parameter has invalid type", () => {
+  test('throws an error if the second parameter has invalid type', () => {
     expect(() => {
       createOptionArrayFromOptionCollection(productJson, false);
     }).toThrow();
@@ -134,11 +118,11 @@ describe("createOptionArrayFromOptionCollection", () => {
     }).toThrow();
 
     expect(() => {
-      createOptionArrayFromOptionCollection(productJson, "");
+      createOptionArrayFromOptionCollection(productJson, '');
     }).toThrow();
 
     expect(() => {
-      createOptionArrayFromOptionCollection(productJson, "test");
+      createOptionArrayFromOptionCollection(productJson, 'test');
     }).toThrow();
 
     expect(() => {
@@ -150,23 +134,23 @@ describe("createOptionArrayFromOptionCollection", () => {
     }).toThrow();
   });
 
-  test("returns an array of options when called with an object of valid keys and values", () => {
+  test('returns an array of options when called with an object of valid keys and values', () => {
     const criteria = [
-      { name: "Size", value: "36" },
-      { name: "Color", value: "Black" }
+      { name: 'Size', value: '36' },
+      { name: 'Color', value: 'Black' }
     ];
     const variant = createOptionArrayFromOptionCollection(
       productJson,
       criteria
     );
-    const expected = ["36", "Black"];
+    const expected = ['36', 'Black'];
     expect(variant).toEqual(expected);
   });
 
   test("returns an empty array when called with an object of unmatched 'name' key", () => {
     const criteria = [
-      { name: "Random", value: "test" },
-      { name: "House", value: "Wall" }
+      { name: 'Random', value: 'test' },
+      { name: 'House', value: 'Wall' }
     ];
     const variant = createOptionArrayFromOptionCollection(
       productJson,
@@ -177,8 +161,8 @@ describe("createOptionArrayFromOptionCollection", () => {
 
   test("throws an error message when called with an object with 'name' key as absent", () => {
     const criteria = [
-      { color: "Red", image: "myimage.jpg" },
-      { property: "Name", value: "Random" }
+      { color: 'Red', image: 'myimage.jpg' },
+      { property: 'Name', value: 'Random' }
     ];
 
     expect(() => {
