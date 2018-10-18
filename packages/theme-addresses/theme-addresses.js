0e1114a49fff4308c97a24b339e0a098f51bb34a
Helen Lin <>
2018-10-18 17:17:27 -0400

    break things up a bit more and add hide option

--- packages/theme-addresses/theme-addresses.js
+++ packages/theme-addresses/theme-addresses.js
@@ -3,11 +3,13 @@
  * @param {Object} countryOptions Country JSON object
  */
 export function CountryProvinceSelector(countryOptions) {
-  this.options = countryOptions
+  this.countryOptions = countryOptions
 }
 
-var buildProvince = function (provinceNodeElement, selectedOption) {
-  var provinces = JSON.parse(selectedOption.getAttribute('data-provinces'));
+/**
+ * Helper function that builds the province selector
+ */
+var buildProvince = function (provinceNodeElement, provinces) {
   var defaultValue = provinceNodeElement.getAttribute('data-default');
 
   provinceNodeElement.innerHTML = provinces.map(function (option) {
@@ -17,28 +19,43 @@ var buildProvince = function (provinceNodeElement, selectedOption) {
 }
 
 /**
+ * Helper function that determins if province selector needs to be build
+ */
+var resolveProvinces = function(countryNodeElement, provinceNodeElement, selectedValue, configrations) {
+  var selectedOption = countryNodeElement.querySelectorAll('option[value="' + selectedValue +'"]')[0];
+  var provinces = JSON.parse(selectedOption.getAttribute('data-provinces'));
+
+  if (provinces.length) {
+    buildProvince(provinceNodeElement, provinces)
+    configrations && configrations.hideClass && provinceNodeElement.classList.remove(configrations.hideClass);
+  }
+}
+
+/**
  * Builds the country and province selector with the given node element
  * @param {Node} countryNodeElement The select Node element for country
  * @param {Node} provinceNodeElement The select Node element for province
+ * @param {Object} configrations Additional settings available
+ *   hideClass        - The classname that will be toggled for province selector
  */
-CountryProvinceSelector.prototype.build = function (countryNodeElement, provinceNodeElement) {
+CountryProvinceSelector.prototype.build = function (countryNodeElement, provinceNodeElement, configrations) {
   var defaultValue = countryNodeElement.getAttribute('data-default');
 
-  countryNodeElement.innerHTML = this.options;
+  countryNodeElement.innerHTML = this.countryOptions;
   countryNodeElement.value = defaultValue;
+  configrations && configrations.hideClass && provinceNodeElement.classList.add(configrations.hideClass);
 
   if (defaultValue) {
-    var selectedOption = countryNodeElement.querySelectorAll('option[value="' + defaultValue +'"]')[0];
-    buildProvince(provinceNodeElement, selectedOption)
+    resolveProvinces(countryNodeElement, provinceNodeElement, defaultValue, configrations)
   }
 
   // Listen for value change on the country select
   countryNodeElement.addEventListener('change', function (event) {
     var target = event.target;
     var selectedValue = target.value;
-    var selectedOption = target.querySelectorAll('option[value="' + selectedValue +'"]')[0];
     
-    provinceNodeElement.value = null
-    buildProvince(provinceNodeElement, selectedOption)
+    provinceNodeElement.innerHTML = ''
+    configrations && configrations.hideClass && provinceNodeElement.classList.add(configrations.hideClass);
+    resolveProvinces(target, provinceNodeElement, selectedValue, configrations)
   });
 }
